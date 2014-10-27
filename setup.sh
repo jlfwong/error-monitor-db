6fae62ee8f0b8dcebc312e5ac8f3e8508e527013
Tom Yedwab <tom@khanacademy.org>
2014-10-26 20:29:42 -0700

    Fix setup.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt-get install redis-server python-pip
+apt-get update
+apt-get -y install redis-server python-pip
 pip install Flask
 pip install oauth2client
 pip install google-api-python-client
