0297fe5c6b36e3d303824b35643a5538fdc075a8
Tom Yedwab <tom@khanacademy.org>
2014-10-26 20:49:34 -0700

    Upstart script for error-monitor-db server

--- setup.sh
+++ setup.sh
@@ -6,3 +6,9 @@ pip install Flask
 pip install oauth2client
 pip install google-api-python-client
 pip install redis
+
+cp /home/ubuntu/error-monitor-db/server.conf /etc/init/error-monitor-db.conf
+chown root:root /etc/init/error-monitor-db.conf
+chmod 644 /etc/init/error-monitor-db.conf
+
+start error-monitor-db
